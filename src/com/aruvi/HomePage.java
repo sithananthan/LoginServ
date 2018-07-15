1a162dc42943570a49bfdbfa2abbf19a7cb14c6a
sithananthan <lsithananthan@gmail.com>
2018-07-15 19:28:17 +0530

    Login using servelt added

---
+++ src/com/aruvi/HomePage.java
@@ -0,0 +1,46 @@
+package com.aruvi;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;  
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+  
+  
+public class HomePage extends HttpServlet {  
+public void doPost(HttpServletRequest request, HttpServletResponse response)  
+        throws ServletException, IOException {  
+  
+    response.setContentType("text/html");  
+    PrintWriter out = response.getWriter();  
+          
+    String n=request.getParameter("uname");  
+    String p=request.getParameter("pwd");
+    System.out.println("error");
+          
+    if(validate(n, p)){  
+        RequestDispatcher requestDispatcher=request.getRequestDispatcher("welcome");  
+        requestDispatcher.forward(request,response);  
+    }  
+    else{  
+        out.print("Sorry username or password error");  
+        RequestDispatcher requestDispatcher=request.getRequestDispatcher("index.html");  
+        requestDispatcher.include(request,response);  
+    }  
+          
+    out.close();  
+    }
+
+	boolean validate(String uname, String password) {
+		boolean loginValue = false;
+		System.out.println("Validate method");
+		if (uname.equals("xsitlak") && password.equals("12345")) loginValue = true;
+		else if (uname.equals("xbalvid") && password.equals("23456")) loginValue = true;
+		
+		return loginValue;
+	}
+	
+}  
